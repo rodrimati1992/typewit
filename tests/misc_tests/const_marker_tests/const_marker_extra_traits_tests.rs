ba7be481637e23c4939dd9f6fd1a0b9cb0b71820
rodrimati1992 <rodrimatt1985@gmail.com>
2026-04-01 19:10:56 -0300

    Fixed support for older versions (?)

--- tests/misc_tests/const_marker_tests/const_marker_extra_traits_tests.rs
+++ tests/misc_tests/const_marker_tests/const_marker_extra_traits_tests.rs
@@ -2,6 +2,10 @@ use typewit::const_marker::U8;
 
 use core::cmp::{Ord, PartialOrd, Ordering};
 
+use core::hash::{BuildHasher, Hasher};
+
+use std::collections::hash_map::RandomState;
+
 
 #[test]
 fn test_partial_cmp() {
@@ -16,17 +20,24 @@ fn test_cmp() {
     assert_eq!(U8::<1>.cmp(&U8::<1>), Ordering::Equal);
 }
 
+
+fn hash_one<T: std::hash::Hash>(this: &mut RandomState, x: T) -> u64 {
+    let mut hasher = this.build_hasher();
+    x.hash(&mut hasher);
+    hasher.finish()
+}
+
 #[test]
 fn test_hash() {
     use core::hash::BuildHasher;
 
-    let bh = std::hash::RandomState::new();
+    let bh = &mut RandomState::new();
 
-    assert_eq!(bh.hash_one(U8::<0>), bh.hash_one(0u8));
-    assert_eq!(bh.hash_one(U8::<1>), bh.hash_one(1u8));
-    assert_eq!(bh.hash_one(U8::<42>), bh.hash_one(42u8));
-    assert_eq!(bh.hash_one(U8::<128>), bh.hash_one(128u8));
-    assert_eq!(bh.hash_one(U8::<255>), bh.hash_one(255u8));
+    assert_eq!(hash_one(bh, U8::<0>), hash_one(bh, 0u8));
+    assert_eq!(hash_one(bh, U8::<1>), hash_one(bh, 1u8));
+    assert_eq!(hash_one(bh, U8::<42>), hash_one(bh, 42u8));
+    assert_eq!(hash_one(bh, U8::<128>), hash_one(bh, 128u8));
+    assert_eq!(hash_one(bh, U8::<255>), hash_one(bh, 255u8));
 }
 
 
