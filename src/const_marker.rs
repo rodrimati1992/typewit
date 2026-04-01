92ffbb22defaf0fc9e821529d96bdb43d22e3959
rodrimati1992 <rodrimatt1985@gmail.com>
2026-04-01 18:14:08 -0300

    Fixed slice implementation of Deserialize for const marker types with a full rewrite
    Added serde tests for more types

--- src/const_marker.rs
+++ src/const_marker.rs
@@ -120,7 +120,8 @@ macro_rules! declare_const_param_type {
 macro_rules! __declare_const_param_type {
     (
         $(#[$struct_docs:meta])*
-        $struct:ident($prim:ty)
+        $struct:ident($prim:ty) 
+        $deser_type:ident,
 
         $(
             $(#[$eq_docs:meta])*
@@ -251,7 +252,7 @@ macro_rules! __declare_const_param_type {
         }
 
         #[cfg(feature = "serde")]
-        crate::const_marker::__declare_const_marker_serde_impls!{$struct($prim)}
+        crate::const_marker::__declare_const_marker_serde_impls!{$struct($prim) $deser_type}
 
 
     };
@@ -259,7 +260,7 @@ macro_rules! __declare_const_param_type {
 
 
 declare_const_param_type!{
-    Bool(bool)
+    Bool(bool) primitive,
 
     /// 
     /// For getting a type witness that
@@ -270,16 +271,17 @@ declare_const_param_type!{
     /// 
     fn equals;
 }
-declare_const_param_type!{Char(char)}
+declare_const_param_type!{Char(char) primitive,}
 
-declare_const_param_type!{U8(u8)}
-declare_const_param_type!{U16(u16)}
-declare_const_param_type!{U32(u32)}
-declare_const_param_type!{U64(u64)}
-declare_const_param_type!{U128(u128)}
+declare_const_param_type!{U8(u8) primitive,}
+declare_const_param_type!{U16(u16) primitive,}
+declare_const_param_type!{U32(u32) primitive,}
+declare_const_param_type!{U64(u64) primitive,}
+declare_const_param_type!{U128(u128) primitive,}
 
 declare_const_param_type!{
     Usize(usize)
+    primitive,
 
     /// # Examples
     /// 
@@ -373,10 +375,10 @@ declare_const_param_type!{
     fn equals;
 }
 
-declare_const_param_type!{I8(i8)}
-declare_const_param_type!{I16(i16)}
-declare_const_param_type!{I32(i32)}
-declare_const_param_type!{I64(i64)}
-declare_const_param_type!{I128(i128)}
-declare_const_param_type!{Isize(isize)}
+declare_const_param_type!{I8(i8) primitive,}
+declare_const_param_type!{I16(i16) primitive,}
+declare_const_param_type!{I32(i32) primitive,}
+declare_const_param_type!{I64(i64) primitive,}
+declare_const_param_type!{I128(i128) primitive,}
+declare_const_param_type!{Isize(isize) primitive,}
 
